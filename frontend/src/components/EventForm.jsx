ea9cc67785a21dbe09985d2851af309f73d9f484
Purushottam <pallipurushotham333@gmail.com>
2025-03-18 15:14:07 +0530

    second commit

---
+++ frontend/src/components/EventForm.jsx
@@ -0,0 +1,16 @@
+// src/components/EventForm.jsx
+
+
+
+function EventForm() {
+
+    return (
+        <>
+            {<input type="text">name</input>}
+            {<input type="date">date</input>}
+            {<input type="text">location</input>}
+        </>
+    );
+}
+
+export default EventForm;
